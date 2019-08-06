2769fcdafe77f90941d08e6346568ab13f2f4bf1
Markezo <Oleksandrson@gmail.com>
2019-08-06 18:10:18 +0300

    add dynamic urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from students import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Students urls
+    path('', views.students_list, name='home'),
+    path('students/add', views.students_add, name='students_add'),
+    re_path(r'^students/(?P<sid>\d+)/edit', views.students_edit, name='students_edit'),
+    re_path(r'^students/(?P<sid>\d+)/delete', views.students_delete, name='students_delete'),
+    # Groups urls
+    path('groups', views.group_list, name='groups'),
+    path('groups/add', views.groups_add, name='groups_add'),
+    re_path(r'^groups/(?P<gid>\d+)/edit', views.groups_edit, name='groups_edit'),
+    re_path(r'^groups/(?P<gid>\d+)/delete', views.groups_delete, name='groups_delete'),
+
 ]
